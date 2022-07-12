5852675a307911df1598c20c65d96666f1da51ce
JaeJun <jejun97@gmail.com>
2022-07-12 18:53:14 +0900

    linked list constructor

---
+++ Linked-Lists/linked-lists.js
@@ -0,0 +1,24 @@
+class Node {
+    constructor (value, next = null) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+
+class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    push(value) {}
+    unshift(value) {}
+    insert(index, value) {}
+}
+
+
+let myLinkedList = new LinkedList(4);
+
