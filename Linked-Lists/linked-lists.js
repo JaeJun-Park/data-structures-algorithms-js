5f50f673ebc8656001a6b1a84dff7fc1afaf7910
JaeJun <jejun97@gmail.com>
2022-07-12 23:40:55 +0900

    linked list pop method

--- Linked-Lists/linked-lists.js
+++ Linked-Lists/linked-lists.js
@@ -1,5 +1,5 @@
 class Node {
-    constructor (value, next = null) {
+    constructor(value, next = null) {
         this.value = value;
         this.next = null;
     }
@@ -14,11 +14,49 @@ class LinkedList {
         this.length = 1;
     }
 
-    push(value) {}
-    unshift(value) {}
-    insert(index, value) {}
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            this.tail.next = newNode;
+        }
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+    pop() {
+        if (!this.head)
+            return undefined;
+
+        let [temp, prev] = [this.head, this.head];
+
+        while (temp.next) {
+            prev = temp;
+            temp = temp.next;
+        }
+        this.tail = prev;
+        this.tail.next = null;
+
+        this.length--;
+        if (this.length == 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return temp;
+
+    }
+    unshift(value) { }
+    insert(index, value) { }
 }
 
 
-let myLinkedList = new LinkedList(4);
+// let myLinkedList = new LinkedList(4);
+// myLinkedList.push(7);
+// myLinkedList.push(10);
 
+// console.log(myLinkedList);
+
+// console.log(myLinkedList.pop());
+// console.log(myLinkedList.pop());
+// console.log(myLinkedList.pop());
