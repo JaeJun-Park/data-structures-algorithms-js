2cade0fc707a529397ffdbbea6eece45ea6ddc87
JaeJun <jejun97@gmail.com>
2022-07-13 16:27:51 +0900

    linked list completed

--- Linked-Lists/linked-lists.js
+++ Linked-Lists/linked-lists.js
@@ -69,7 +69,69 @@ class LinkedList {
         return temp;
     }
 
-    Set()
+    get(index) {
+        if (index < 0 || index >= this.length) return undefined;
 
-    insert(index, value) { }
+        let temp = this.head;
+        while (index > 0) {
+            temp = temp.next;
+            index--;
+        }
+        return temp;
+    }
+
+    Set(index, value) {
+        let temp = this.get(index)
+        if (temp) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    insert(index, value) {
+        if (index == 0) return this.unshift(value);
+        if (index == this.length) return this.push(value);
+        if (index < 0 || index > this.length) return false;
+
+        const newNode = new Node(value);
+        const temp = this.get(index - 1);
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+        this.length++;
+        return true;
+    }
+
+    remove(index) {
+        if (index==0)return this.shift();
+        if(index==this.length-1) return this.pop();
+        if (index < 0 || index >= this.length) return undefined;
+
+        const before = this.get(index-1);
+        const temp = before.next;
+
+        before.next = temp.next;
+        temp.next = null;
+        this.length--;
+        return temp;
+    }
+
+    reverse() {
+        let temp = this.head;
+        this.head = this.tail;
+        this.tail = temp;
+        let next = temp.next;
+        let prev = null;
+
+        for (let i = 0; i < this.length; i++) {
+            next = temp.next;
+            temp.next = prev;
+            prev = temp; 
+            temp = next;
+        }
+
+        return this;
+    }
 }
+
